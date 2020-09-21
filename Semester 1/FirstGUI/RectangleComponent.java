af39c1da75e25e4ac38ff5817b3df073ba809ee4
Elun4705 <36207847+Elun4705@users.noreply.github.com>
2020-09-21 11:56:20 -0600

    Almost finished!

--- Semester 1/FirstGUI/RectangleComponent.java
+++ Semester 1/FirstGUI/RectangleComponent.java
@@ -9,10 +9,10 @@ public class RectangleComponent extends JComponent{
   public void paintComponent(Graphics g){
     Day today = new Day();
     int year = today.getYear();
-    Day dadBday = new Day(1974, 12, 18);
-    Day momBday = new Day(1978, 1, 15);
-    Day broBday = new Day(2007, 11, 12);
-    Day meBday = new Day(2003, 1, 8);
+    Day dadBday = new Day(year - 1, 12, 18);
+    Day momBday = new Day(year - 1, 13, 15);
+    Day broBday = new Day(year - 1, 11, 12);
+    Day meBday = new Day(year -1, 13, 8);
 
     int dBday = 365 - today.daysFrom(dadBday);
     int mBday = 365 - today.daysFrom(momBday);
@@ -21,10 +21,10 @@ public class RectangleComponent extends JComponent{
 
 
     Graphics2D g2 = (Graphics2D) g;
-    Rectangle box1 = new Rectangle(5, 10, dBday, 30);
-    Rectangle box2 = new Rectangle(25, 10, mBday, 30);
-    Rectangle box3 = new Rectangle(45, 10, bBday, 30);
-    Rectangle box4 = new Rectangle(65, 10, myBday, 30);
+    Rectangle box1 = new Rectangle(50, 100, dBday, 30);
+    Rectangle box2 = new Rectangle(50, 200, mBday, 30);
+    Rectangle box3 = new Rectangle(50, 300, bBday, 30);
+    Rectangle box4 = new Rectangle(50, 400, myBday, 30);
 
     g2.draw(box1);
     g2.draw(box2);
