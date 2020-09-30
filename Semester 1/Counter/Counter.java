e08460f4e267b1adc5f84f423212b78b5748298e
Elun4705 <36207847+Elun4705@users.noreply.github.com>
2020-09-30 10:49:09 -0600

    Starter XXX

---
+++ Semester 1/Counter/Counter.java
@@ -0,0 +1,19 @@
+public class Counter
+{
+   private int value;
+
+   public int getValue()
+   {
+      return value;
+   }
+
+   public void click()
+   {
+      value = value + 1;
+   }
+
+   public void reset()
+   {
+      value = 0;
+   }
+}
