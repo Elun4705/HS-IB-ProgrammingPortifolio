c6515675b9abc88c91fff23fa473959e032d741b
Elun4705 <36207847+Elun4705@users.noreply.github.com>
2020-10-06 12:01:25 -0600

    Booyah!

---
+++ Semester 1/Bank Account/BankAccount.java
@@ -0,0 +1,40 @@
+/**
+* This class just displays the actual result
+* @author  Emmanuel Luna
+* @version 1.0
+* @since   2020-10-06
+*/
+
+public class BankAccount {
+  private double balance;
+  private double transactionFee;
+  private double monthlyCharge;
+  private int    transactionToFee;
+
+  public Account() {
+    this.balance = 0;
+    this.transactionFee = 0.10;
+    this.monthlyCharge = 0;
+  }
+
+  public getBalance() {
+    return this.balance;
+  }
+
+  public deposit(double amount) {
+    this.balance += amount;
+  }
+
+  public withdrawal(double amount) {
+    this.balance -= amount;
+  }
+
+  public setTransactionFee(double fee) {
+
+  }
+
+  public deductMonthlyCharge() {
+
+  }
+
+}
---
+++ Semester 1/Bank Account/BankAccountTester.java
@@ -0,0 +1,36 @@
+/**
+* This class just displays the actual result of the Bank Account
+* @author  Emmanuel Luna
+* @version 1.0
+* @since   2020-10-06
+*/
+
+import java.util.Scanner;
+
+public class BankAccountTester {
+  public static void main(String[] args) {
+    BankAccount account = new BankAccount();
+
+    System.out.print("  Hello and welcome to your Bank Account!  ");
+    System.out.print("         How can I help you today?         ");
+    System.out.print("  1 - Balance 2 - Withdrawal 3 - Deposit   ");
+
+    switch (){
+      case 1:
+
+        break;
+
+      case 2:
+
+        break;
+
+      case 3:
+
+        break;
+
+      default:
+
+        break;
+    }
+  }
+}
